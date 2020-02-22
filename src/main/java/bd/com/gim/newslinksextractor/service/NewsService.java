3fbf5f6d961d4a7d72a29d14c4d0920b59b2735f
Tanbirul Hashan <tanu.tanbirul@gmail.com>
2020-02-22 14:04:52 +0600

    News service stubs created

---
+++ src/main/java/bd/com/gim/newslinksextractor/service/NewsService.java
@@ -0,0 +1,12 @@
+package bd.com.gim.newslinksextractor.service;
+
+import bd.com.gim.newslinksextractor.model.News;
+/**
+ * News Service interface
+ * 
+ * @author Tanbirul Hashan
+ * @since 2020-02-22
+ */
+public interface NewsService{
+	News insert(News ob); 
+}
---
+++ src/main/java/bd/com/gim/newslinksextractor/service/NewsServiceImpl.java
@@ -0,0 +1,17 @@
+package bd.com.gim.newslinksextractor.service;
+
+import bd.com.gim.newslinksextractor.model.News;
+/**
+ * News Service Implementation class
+ * 
+ * @author Tanbirul Hashan
+ * @since 2020-02-22
+ */
+public class NewsServiceImpl implements NewsService{
+
+	@Override
+	public News insert(News ob) {
+		return null;
+	}
+
+}
