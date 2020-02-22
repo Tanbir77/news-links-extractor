4188e63745090a7e3e75d003f595d4a49d66e12a
Tanbirul Hashan <tanu.tanbirul@gmail.com>
2020-02-22 14:16:55 +0600

    News service implemented

--- src/main/java/bd/com/gim/newslinksextractor/service/NewsServiceImpl.java
+++ src/main/java/bd/com/gim/newslinksextractor/service/NewsServiceImpl.java
@@ -1,5 +1,13 @@
 package bd.com.gim.newslinksextractor.service;
 
+import javax.persistence.EntityExistsException;
+import javax.persistence.EntityManager;
+import javax.persistence.TransactionRequiredException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import bd.com.gim.newslinksextractor.PersistenceManager;
 import bd.com.gim.newslinksextractor.model.News;
 /**
  * News Service Implementation class
@@ -8,10 +16,25 @@ import bd.com.gim.newslinksextractor.model.News;
  * @since 2020-02-22
  */
 public class NewsServiceImpl implements NewsService{
-
+	private final Logger log = LoggerFactory.getLogger(getClass());
+	private static EntityManager em;
+	
+	static {
+		em = PersistenceManager.INSTANCE.getEntityManager();
+	}
 	@Override
 	public News insert(News ob) {
-		return null;
+		try {
+			em.getTransaction().begin();
+			em.persist(ob);
+			em.getTransaction().commit();
+			return ob;
+		} catch (IllegalStateException | EntityExistsException | IllegalArgumentException
+				| TransactionRequiredException e) {
+			log.warn(String.format("\"%s\" failed to save", e));
+			return null;
+		}
+
 	}
 
 }
